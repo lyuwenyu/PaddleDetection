e3c97a09518a08658574e64a145aed273e2c7372
lyuwenyu <wenyu.lyu@gmail.com>
2022-05-28 17:19:57 +0800

    update run bash

--- run.sh
+++ run.sh
@@ -1,9 +1,21 @@
 
-ps aux | grep "tools/train.py" | awk '{print $2}' | xargs kill -9 
+# ps aux | grep "tools/train.py" | awk '{print $2}' | xargs kill -9 
 
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7                                                                         
+export FLAGS_START_PORT=17000      
+                                                                             
+                                                                                     
+unset TRAINER_IP_LIST                                                                                               
+unset TRAINER_INSTANCES                                                                                             
+unset PADDLE_TRAINERS                                                                                               
+unset TRAINER_IP_PORT_LIST                                                                                          
+unset TRAINERS                                                                                                      
+unset TRAINERS_NUM                                                                                                  
+unset TRAINER_INSTANCES_NUM                                                                                         
+unset TRAINER_HOSTS                                                                                                 
 
 fleetrun \
---ips="ip1,ip2,ip3" \
+--ips="10.127.6.17,10.127.5.142,10.127.45.13,10.127.44.151" \
 --selected_gpu 0,1,2,3,4,5,6,7 \
 tools/train.py \
 -c configs/lst/lst_yoloe_l_3x.yml \
