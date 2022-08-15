61c3591b8c18b1fb6b2a0f9781e104e122f68ff0
lyuwenyu <wenyu.lyu@gmail.com>
2022-08-15 18:52:32 +0800

    update

--- ppdet/modeling/heads/cascade_head.py
+++ ppdet/modeling/heads/cascade_head.py
@@ -246,21 +246,15 @@ class CascadeHead(BBoxHead):
             if not self.reg_class_agnostic and i < self.num_cascade_stages - 1:
                 deltas = deltas.reshape([deltas.shape[0], self.num_classes, 4])
                 labels = scores[:, :-1].argmax(axis=-1)
+                deltas = deltas[paddle.arange(deltas.shape[0]), labels]
 
-                if self.training:
-                    deltas = deltas[paddle.arange(deltas.shape[0]), labels]
-                else:
-                    # deltas = deltas[(deltas * F.one_hot(
-                    #     labels, num_classes=self.num_classes).unsqueeze(-1) != 0
-                    #                  ).nonzero(as_tuple=True)].reshape(
-                    #                      [deltas.shape[0], 4])
-                    ii = paddle.arange(deltas.shape[0])
-                    deltas = deltas[ii, labels]
-
-                    # deltas = deltas[(deltas * F.one_hot(
-                    #     labels, num_classes=self.num_classes).unsqueeze(-1) != 0
-                    #                  ).nonzero(as_tuple=True)].reshape(
-                    #                      [deltas.shape[0], 4])
+                # if self.training:
+                #     deltas = deltas[paddle.arange(deltas.shape[0]), labels]
+                # else:
+                #     deltas = deltas[(deltas * F.one_hot(
+                #         labels, num_classes=self.num_classes).unsqueeze(-1) != 0
+                #                      ).nonzero(as_tuple=True)].reshape(
+                #                          [deltas.shape[0], 4])
 
             head_out_list.append([scores, deltas, rois])
             pred_bbox = self._get_pred_bbox(deltas, rois, self.bbox_weight[i])
