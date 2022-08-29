98443922d538c4256ebaf17f37d415b7b0461af6
lyuwenyu <wenyu.lyu@gmail.com>
2022-08-29 15:41:55 +0800

    updat

--- command.sh
+++ command.sh
@@ -1,3 +1,4 @@
 # python tools/export_model.py -c configs/vitdet/ppyole_vit_base_coco.yml
 # python deploy/python/infer.py  --run_mode=paddle --device=GPU --threshold=0.5 --output_dir=python_infer_output --image_dir=./demo --run_benchmark True --model_dir=output_inference/
 
+# python -m paddle.distributed.launch --log_dir=log --gpus 0,1,2,3,4,5,6,7 tools/train.py -c configs/vitdet/ppyole_vit_base_60e_coco.yml --eval &> train.txt 2>&1 &
