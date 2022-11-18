a6c7fea3e25b8db4f1005bf58609551852b743f8
lyuwenyu <wenyu.lyu@gmail.com>
2022-11-18 10:25:17 +0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 
 fleetrun --gpus 0,1,2,3 tools/train.py -c ./configs/dino/dino_r50_1x_coco.yml --eval &>dino_r50_1x_coco.txt 2>&1 &
+
+fleetrun --gpus 0,1,2,3 tools/train.py -c ./configs/dino/dino_r50_yoloe_reader_1x_coco.yml --eval &>dino_r50_yoloe_reader_1x_coco.txt 2>&1 &
