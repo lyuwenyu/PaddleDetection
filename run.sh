2658b4830b2e9fa5cfb9f7f0da3ade47f38a7a80
lyuwenyu <wenyu.lyu@gmail.com>
2022-11-29 11:16:36 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+# ps aux | grep "tools/train.py" | awk '{print $2}' | xargs kill -9 
+
+# python tools/export_model.py -c configs/dino/dino_r50_1x_coco.yml -o weights=./4.pdparams trt=True
+# paddle2onnx --model_dir=./output_inference/dino_r50_1x_coco/ --model_filename=model.pdmodel --params_filename=model.pdiparams  --opset_version 16 --save_file dino.onnx
+# onnxsim dino.onnx dino_new.onnx  --overwrite-input-shape im_shape:1,2 image:1,3,640,640 scale_factor:1,2
+# ../software/TensorRT-8.5.1.7/bin/trtexec --onnx=./dino_new.onnx --workspace=4096 --avgRuns=100  --fp16
+
 
 fleetrun --gpus 0,1,2,3 tools/train.py -c ./configs/dino/dino_r50_1x_coco.yml --eval &>dino_r50_1x_coco.txt 2>&1 &
 
