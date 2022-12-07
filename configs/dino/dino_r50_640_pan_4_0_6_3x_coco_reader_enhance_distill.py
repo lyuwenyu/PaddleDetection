394f31891c9af397080763e2a45918b273fb9a45
lyuwenyu <wenyu.lyu@gmail.com>
2022-12-07 20:13:00 +0800

    distill

---
+++ configs/dino/dino_r50_640_pan_4_0_6_3x_coco_reader_enhance_distill.py
@@ -0,0 +1,17 @@
+import paddle
+from ppdet.core.workspace import load_config, merge_config
+from ppdet.core.workspace import create
+
+cfg = load_config(
+    './configs/dino/dino_r50_640_pan_4_0_6_3x_coco_reader_enhance_distill.yml')
+
+# model = create('DETR')
+# print(model)
+# print(cfg.teacher['architecture'])
+
+teacher = create(cfg.teacher['architecture'])
+
+if 'pretrain_weights' in cfg.teacher:
+    state = paddle.load(cfg.teacher['pretrain_weights'])
+    teacher.set_state_dict(state)
+    teacher.eval()
