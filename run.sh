33d2e50357cdf8410507dd5978bef09566d47794
lyuwenyu <wenyu.lyu@gmail.com>
2023-02-03 16:36:25 +0800

    update

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ fleetrun --gpus 0,1,2,3 tools/train.py -c ./configs/dino/dino_r50_1x_coco.yml --
 
 fleetrun --gpus 0,1,2,3 tools/train.py -c ./configs/dino/dino_r50_yoloe_reader_1x_coco.yml --eval &>dino_r50_yoloe_reader_1x_coco.txt 2>&1 &
 
+fleetrun --gpus 0,1,2,3 tools/train.py -c ./configs/dino/dino_r50_yoloe_reader_1x_coco.yml --eval --fleet &>dino_r50_yoloe_reader_1x_coco.txt 2>&1 &
 
 
 # fleetrun --log_dir=./logs/ --gpus 0,1,2,3 tools/train.py -c ./configs/dino/dino_r50_640_pan_4_0_6_3x_coco.yml --eval &>dino_r50_640_pan_4_0_6_3x_coco_q_300_vd_ema_sync_bn.txt 2>&1 &
