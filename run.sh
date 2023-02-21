1e97fd7b09be23845e4cc9960912a049daea774c
lyuwenyu <wenyu.lyu@gmail.com>
2023-02-21 16:52:37 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 
 
- python tools/export_model.py -c configs/dino/dino_base_sim_speed.yml -o weights=neck_1024.pdparams trt=True
+python tools/export_model.py -c configs/dino/dino_base_sim_speed.yml -o weights=neck_1024.pdparams trt=True
+
+rm -f shape_range_info.pbtxt
+
+python deploy/python/infer.py --run_mode=trt_fp16 --device=GPU --run_benchmark=True --threshold=0.5 --output_dir=python_infer_output --image_file=./demo/000000014439_640x640.jpg --model_dir=output_inference/dino_base_sim_speed/
 
 python deploy/python/infer.py --run_mode=trt_fp16 --device=GPU --run_benchmark=True --threshold=0.5 --output_dir=python_infer_output --image_file=./demo/000000014439_640x640.jpg --model_dir=output_inference/dino_base_sim_speed/
