ba1baf29d0ddd6435743f032b5943bd6df85e431
lyuwenyu <wenyu.lyu@gmail.com>
2023-02-21 16:44:39 +0800

    update

---
+++ run.sh
@@ -0,0 +1,5 @@
+
+
+
+
+python deploy/python/infer.py --run_mode=trt_fp16 --device=GPU --run_benchmark=True --threshold=0.5 --output_dir=python_infer_output --image_file=./demo/000000014439_640x640.jpg --model_dir=output_inference/dino_base_sim/
